b7c648eeb50d0ac9d75fd94e2b8041e1d2489d34
ilaahe04 <ilaha.hybtv04@gmail.com>
2024-01-12 05:21:50 -0800

    Implement 'pint' function to print the top value of the stack.

--- main.h
+++ main.h
@@ -36,5 +36,6 @@ void free_lineptr(int status, void *arg);
 stack_t *add_node(stack_t **stack, const int n);
 int main(int argc, char *argv[]);
 void fs_close(int status, void *arg);
+void pint(stack_t **stack, unsigned int line_number);
 
 #endif
