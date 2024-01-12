9e1273f52c55a64383da0924754f11abb40af11d
ilaahe04 <ilaha.hybtv04@gmail.com>
2024-01-12 12:21:14 -0800

    added syntax

--- main.h
+++ main.h
@@ -48,7 +48,7 @@ int main(int argc, char *argv[]);
 void fs_close(int status, void *arg);
 void pint(stack_t **stack, unsigned int line_number);
 void pall(stack_t **stack, unsigned int line_number);
-void push(stack_t **stack, unsigned int line_number);
+int push(stack_t **stack, char *op, unsigned int line_number);
 static int check_for_digit(char *arg);
 
 #endif
