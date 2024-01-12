f9afee06c713a5455566a5e4fbc4cd5e7bc6e8de
ilaahe04 <ilaha.hybtv04@gmail.com>
2024-01-12 13:02:00 -0800

    add file name

--- execute.c
+++ execute.c
@@ -1,18 +1,18 @@
 #include "main.h"
 /**
  * execute - Something useful
- * @file: Something more useful
+ * @fd: Something more useful
  * @stack: Something more useful
  *
  * Return: Something much more usefull
  */
-int execute(FILE *file, stack_t **stack)
+int execute(FILE *fd, stack_t **stack)
 {
 	char *buf = NULL, *tok = NULL;
 	size_t size = 0;
 	int line_count = 0, i = 0;
 
-	while (getline(&buf, &size, file) != -1 && i != 1)
+	while (getline(&buf, &size, fd) != -1 && i != 1)
 	{
 		line_count++;
 		tok = strtok(buf, " \n\t\r$");
