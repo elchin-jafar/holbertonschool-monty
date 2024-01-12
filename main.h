9b735b710661b9778f9d33c6996fd7fcf730c523
ilaahe04 <ilaha.hybtv04@gmail.com>
2024-01-12 11:44:59 -0800

    add syntax

--- main.h
+++ main.h
@@ -46,6 +46,8 @@ stack_t *add_node(stack_t **stack, const int n);
 int main(int argc, char *argv[]);
 void fs_close(int status, void *arg);
 void pint(stack_t **stack, unsigned int line_number);
-
+void pall(stack_t **stack, unsigned int line_number);
+void push(stack_t **stack, unsigned int line_number);
+static int check_for_digit(char *arg);
 
 #endif
