eca04a111c2e5f5fa1631af2be6b671ccddd9862
Elchin <elcinceferli@gmail.com>
2024-01-13 09:15:23 -0800

    satisfy betty

--- push.c
+++ push.c
@@ -3,6 +3,7 @@
 /**
  * push - push an integer onto the stack
  * @stack: double pointer to the beginning of the stack
+ * @op: opcode
  * @line_number: script line number
  *
  * Return: 0
@@ -10,30 +11,31 @@
 
 int push(stack_t **stack, char *op, unsigned int line_number)
 {
-    stack_t *new;
+	stack_t *new;
 
-    new = malloc(sizeof(stack_t));
-    if (new == NULL)
-    {
-        dprintf(STDERR_FILENO, "Error: malloc failed\n");
-        return (1);
-    }
-    if (convert(op) == 1 && op != NULL)
-    {
-        new->n = atoi(op);
-        new->prev = NULL;
-        new->next = *stack;
-        if (*stack != NULL)
-        {
-            (*stack)->prev = new;
-        }
-        *stack = new;
-    }
-    else
-    {
-        dprintf(STDERR_FILENO, "L%u: usage: push integer\n", line_number);
-        free(new);
-        return (1);
-    }
-    return (0);
+
+	new = malloc(sizeof(stack_t));
+	if (new == NULL)
+	{
+		dprintf(STDERR_FILENO, "Error: malloc failed\n");
+		return (1);
+	}
+	if (convert(op) == 1 && op != NULL)
+	{
+		new->n = atoi(op);
+		new->prev = NULL;
+		new->next = *stack;
+		if (*stack != NULL)
+		{
+			(*stack)->prev = new;
+		}
+		*stack = new;
+	}
+	else
+	{
+		dprintf(STDERR_FILENO, "L%u: usage: push integer\n", line_number);
+		free(new);
+		return (1);
+	}
+	return (0);
 }
